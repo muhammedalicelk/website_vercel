28000bb547470d4550e1d41a6308ecec3afd3458
muhammedalicelk <muhammedalicelk@gmail.com>
2025-12-28 22:07:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const SONGS = [
      ===================== */
   YT('La Mentira', 'P8BLkulZGX8', { tags: ['Romantik', 'İspanyolca'] }),
   YT('Love In Portofino', 'AKDLoUSaPV8', { tags: ['Romantik', 'İspanyolca'] }),
-  YT('Bésame Mucho', 'M4z6xdu1iX8', { tags: ['Romantik', 'İspanyolca'] }),
+  YT('Besame Mucho', 'M4z6xdu1iX8', { tags: ['Romantik', 'İspanyolca'] }),
   YT('Historia de un Amor', 'HzjE33U_gy8', { tags: ['Romantik', 'İspanyolca'] }),
 
   /* =====================
